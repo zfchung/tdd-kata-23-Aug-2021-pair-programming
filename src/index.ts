dfc67e02940c80fb66dab106ee35b73633ea9957
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-23 17:27:51 +0800

    changed delimiters to a common to process

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export function add(input: string): number {
   const newValue = input ? input : "0";
-  const delimiter = /\n|,/;
-  const valueList = newValue.split(delimiter);
+  const commonDelimiterValue = newValue.replaceAll("\n", ",");
+  const valueList = commonDelimiterValue.split(",");
   const valueListNumbers = valueList.map(Number);
   const sumValueList = sumListItems(valueListNumbers);
   return sumValueList;
