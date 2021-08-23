51d1557f27e8bb5cbaf71edd32948445fff230e1
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-23 17:06:51 +0800

    Expresses inteent in the operations of add()

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 export function add(input: string): number {
-  const newValue = input
-    ? input
-        .split(",")
-        .map(Number)
-        .reduce((accumulator, currentValue) => accumulator + currentValue)
-    : 0;
-  return newValue;
+  const newValue = input ? input : "0";
+  const valueList = newValue.split(",");
+  const valueListNumbers = valueList.map(Number);
+  const sumValueList =
+    valueListNumbers[0] + (valueListNumbers[1] ? valueListNumbers[1] : 0);
+  return sumValueList;
 }
